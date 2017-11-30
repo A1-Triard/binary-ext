9d491c5569aeed0b318098fecb5f20c9c3495681
A1 <internalmike@gmail.com>
2017-11-30 21:38:55 +0300

    Spec

---
+++ test/Data/Binary/Get/Ext/Spec.hs
@@ -0,0 +1,4 @@
+module Data.Binary.Get.Ext.Spec where
+
+#define TESTS
+#include <haskell>
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,7 @@
+#define TESTS
+#include <haskell>
+
+import qualified Data.Binary.Get.Ext.Spec
+
 main :: IO ()
 main = putStrLn "Test suite not yet implemented"
