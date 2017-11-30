d14f4006e483a7c37030c54daf13f2ffe23a601f
A1 <internalmike@gmail.com>
2017-11-30 23:00:07 +0300

    tests

--- test/Data/Binary/Get/Ext/Spec.hs
+++ test/Data/Binary/Get/Ext/Spec.hs
@@ -1,4 +1,31 @@
-module Data.Binary.Get.Ext.Spec where
+--
+-- Copyright 2017 Warlock <internalmike@gmail.com>
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+
+module Data.Binary.Get.Ext.Spec
+  ( tests
+  ) where
 
 #define TESTS
 #include <haskell>
+
+
+tests :: Test
+tests = TestList
+  [ TestCase getBytes
+  ]
+
+getBytes :: Assertion
+getBytes = return ()
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,7 +1,28 @@
+--
+-- Copyright 2017 Warlock <internalmike@gmail.com>
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+
 #define TESTS
 #include <haskell>
 
 import qualified Data.Binary.Get.Ext.Spec
 
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = void $ runTestTT tests
+
+tests :: Test
+tests = TestList
+  [ Data.Binary.Get.Ext.Spec.tests
+  ]
